03874190a253524a82adffef42ec8a5e76eae276
casper <marketmg126@gmail.com>
2026-08-11 04:37:09 +0700

    Update and rename 00.php to 0.php

--- 0.php
+++ 0.php
@@ -1,10 +1,8 @@
 RewriteEngine On
 
 RewriteCond %{HTTP_HOST} ^journal\.stieamsir\.ac\.id$ [NC]
-RewriteRule ^(.*)$ https://journalstih.amsir.ac.id/$1 [R=301,L]
 RewriteCond %{HTTP_HOST} ^journal\.stieamsir\.ac\.id$ [OR]
 RewriteCond %{HTTP_HOST} ^www\.journal\.stieamsir\.ac\.id$
-RewriteRule ^/?$ "https\:\/\/journalstih\.amsir\.ac\.id\/" [R=301,L]
 
 # php -- BEGIN cPanel-generated handler, do not edit
 # Set the “ea-php72” package as the default “PHP” programming language.
