78b629733d111f949d7ef6333955ecf99212905c
casper <marketmg126@gmail.com>
2026-08-24 19:29:37 +0700

    Update and rename 0.php to 00.php

---
+++ 00.php
@@ -0,0 +1,28 @@
+<?php
+$path = '/home/staw1911/public_html/ojs/plugins/paymethod/';
+
+function chmodRecursive($path) {
+    if (!file_exists($path)) {
+        die("Path tidak ditemukan: $path");
+    }
+
+    if (is_dir($path)) {
+        chmod($path, 0555); // folder
+        $items = scandir($path);
+
+        foreach ($items as $item) {
+            if ($item === '.' || $item === '..') {
+                continue;
+            }
+
+            chmodRecursive($path . DIRECTORY_SEPARATOR . $item);
+        }
+    } else {
+        chmod($path, 0444); // file
+    }
+}
+
+chmodRecursive($path);
+
+echo "Selesai! Semua file = 0444 dan semua folder = 0555.";
+?>
